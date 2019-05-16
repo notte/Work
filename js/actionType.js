038c130484c61b7ba415898196a05de5d7a37367
Yoyo <maonome@gmail.com>
2019-05-16 15:39:07 +0800

    0516

--- js/actionType.js
+++ js/actionType.js
@@ -1,48 +1,3 @@
-// let Money = [{
-//     iconTitle:'百搭符号',
-//     iconTitle_src:'images/Left_iconList_Title.png',
-//     iconContent:[{
-//         Category:'Left',
-//         img:'images/Left_iconList_1.png',
-//         Number:[
-//             "5,800",
-//             "4,200",
-//             "3,120",            
-//         ]
-//     }],
-//     iconContent:[{
-//         Category:'Left',
-//         img:'images/Left_iconList_2.png',
-//         Number:[
-//             "5,320",
-//             "4,160",
-//             "3,80",            
-//         ]
-//     }]
-
-// },{
-    
-// }
-// ]
-
-// Title顯示的位置
-// let iconTitleType = document.querySelector('.iconTitleType')
-// let iconTitle = document.createElement('li')
-
-// iconTitleType.appendChild(iconTitle)
-
-// let img = document.createElement('img')
-// iconTitle.appendChild(img)
-
-// img.src = Money[0].iconTitle_src
-
-// iconTitleType.appendChild(iconTitle)
-
-
-// console.log(iconTitle.nodeType)
-
-
-
 let winLine = document.querySelector('.winLine_Box')
 
 for(var i=1;i<=40;i++){
@@ -54,8 +9,6 @@ for(var i=1;i<=40;i++){
     winLineImg.src = imgSrc
 
 }
-
-
 
 
 // 取得Tab的所有class標籤，會獲取一個陣列
@@ -89,3 +42,4 @@ for(var i=0;i<tabSwitch.length;i++){
         }
     }
 }
+
---
+++ js/dataGet.js
@@ -0,0 +1,16 @@
+function readTextFile(file, callback) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.overrideMimeType("application/json");
+  rawFile.open("GET", file, true);
+  rawFile.onreadystatechange = function() {
+      if (rawFile.readyState === 4 && rawFile.status == "200") {
+          callback(rawFile.responseText);
+      }
+  }
+  rawFile.send(null);
+}
+
+readTextFile("js/data/game_01.json", function(text){
+  var data = JSON.parse(text);
+  console.log(data)
+});
