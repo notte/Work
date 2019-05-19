68c805f6aa7791cabd216fe24549c552184a3513
notte <https://github.com/notte/page-portfolio-2.git>
2019-05-20 00:35:47 +0800

    0520new

--- js/data.js
+++ js/data.js
@@ -6,15 +6,16 @@ $.ajax({
 
 
 
-    function CreateTitle (data,a){
-      $(".IconList"+ a ).append(
+    function CreateTitle (data,a,c){
+      $(".IconList"+c ).append(
         "<li><img src="+ data[a].iconTitleSrc +"></li>" +
-        "<li>" + data[a].TitleName  + "</li>"
+        "<li>" + data[a].TitleName  + "</li>" +
+        "<li>" + data[a].TitleName1  + "</li>"
       );
     }
 
-    function CreateIcon (data,a){
-      $("#contentIcon"+ a ).append(
+    function CreateIcon (data,a,c){
+      $(".IconBox"+c).append(
         "<ul class='iconType'>"+
             "<li><img src="+ data[a].iconSrc + "></li>"+
             "<li class='MoreData'>"+
@@ -39,47 +40,51 @@ $.ajax({
             "</li>" +
       "</ul>");
     }
- 
+
+    function textBox(data,a){
+
+      var TextUL = createElement('ul')
+      var TextLI = createElement('li')
+
+    }
 
     // 整個遊戲資料
     var data = response
+    // 複製完整的data到新的物件
+    var Obj = data.slice(0)
     
-    // [0]為第一區塊，取得icon列表
-    // IconList為左列(0)或者右列(1)
-    var Obj = data[0].IconList0
-    // console.log(Obj)
+    // [0]為第一區塊，取得icon列表，IconList為左列(0)或者右列(1)
+    var ObjCut = Obj[0].IconList0
     // 取得刪除外層標題之後的內容
-    var ObjName = Object.values(Obj)
-    // console.log(ObjName)
-
-    // icon標題物件
-    // console.log(ObjName[0])
-    // // 標題字
-    // console.log(ObjName[0].TitleName)
-    // // 標題圖
-    // console.log(ObjName[0].iconTitleSrc)
-
+    var ObjName = Object.values(ObjCut)
     // 加到網頁裡
-    CreateTitle(ObjName,0)
-
+    CreateTitle(ObjName,0,0)
     // 處理完後刪除標題
     ObjName.splice(0,1)
-    // 取得剩餘列表內容
-    // console.log(ObjName)
-
     // 逐步取得icon圖片及數字陣列
     for(i=0;i<ObjName.length;i++){
-      CreateIcon(ObjName
-        )
+      CreateIcon(ObjName,i,0)
+    }
+
+    ObjName = ""
+    ObjCut = ""
+
+    ObjCut = Obj[0].IconList1
+    ObjName = Object.values(ObjCut)
+    CreateTitle(ObjName,0,1)
+    ObjName.splice(0,1)
+
+    for(i=0;i<ObjName.length;i++){
+      CreateIcon(ObjName,i,1)
     }
 
 
 
+    // TextBox1
+    for(var i=0;i<Obj.length;i++){
+      
+    }
+
+
   
 })
-
-
-
-
-
-
